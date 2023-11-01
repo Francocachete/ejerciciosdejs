eb153cfd22a51642d7ae2cb4242dfa4897d0ace7
Franco Iñigo <franco.millo77@gmail.com>
2023-10-31 22:02:51 -0300

    agregue los demas ejercicios y llegue hasta el 8

---
+++ js/ejercicio8.js
@@ -0,0 +1,9 @@
+let num = parseInt(prompt("ingresa un numero"));
+if(num % 2 === 0){
+    document.write("el " + num + " es divisible por 2");
+} else{
+    document.write("el " + num + " no es divisible por 2");
+}
+
+
+
